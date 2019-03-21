ae86429c73ff28dc6f2815dacec99f8bfb83d61b
Stahl4121 <Stahl4121@users.noreply.github.com>
2019-03-21 16:40:11 -0400

    Comments for DFS Algorithms

--- src/DFSAlgorithms.java
+++ src/DFSAlgorithms.java
@@ -1,5 +1,7 @@
 
 /**
+ * Contains the DFS algorithms (brute force, w/ backtracking, w/ forward checking)
+ * for solving the n-queens problem.
  * 
  * @authors Sarah Calvis, Logan Stahl, Miriam Tan
  *
@@ -20,29 +22,39 @@ public class DFSAlgorithms {
 	}
 
 	/**
+	 * Recursive method for plain DFS, with no thinking. 
+	 * Branches on every node, only checking if the board 
+	 * is valid at the max depth.
 	 * 
-	 * @param board
-	 * @param depth
-	 * @return
+	 * @param 	board	the initial board to search for a solution
+	 * @param 	depth   the current depth of the board being searched
+	 * @return	true,	if a solution has been found
 	 */
 	public boolean DFS(nQueenBoard board, int depth) {
+
+		//Increment number of nodes expanded
 		numNodes++;
 
+		//Check if the board is a solution
+		//If so, print the solution.
 		if(board.getCollisions() == 0) {
 			System.out.println("\nSolution found using DFS: ");
 			System.out.println(board.toString());
-
-			System.out.println();
-
 			return true;
 		}
 
+		//If board is not a solution, but at the bottom of the
+		//tree, it is not a solution.
 		if (depth == board.getSize()) {
 			return false;
 		}
 		else {
+			//Loop through all positions of a queen at a given depth
 			for (int i = 0; i < board.getSize(); i++) {
+				//Create a new board modified at one position
 				nQueenBoard b = new nQueenBoard(board, depth, i);
+				
+				//Expand the node with a recursive call
 				if (DFS(b, depth+1)) {
 					return true;
 				}
@@ -52,31 +64,46 @@ public class DFSAlgorithms {
 		}
 	}
 
+
 	/**
+	 * Recursive method for DFS with backtracking. 
+	 * Before expanding a node, this implementation
+	 * checks if there are already existing conflicts.
+	 * That is, it prunes the domain of the queen being placed
+	 * based on previously placed queens.
 	 * 
-	 * @param board
-	 * @param depth
-	 * @return
+	 * @param 	board	the initial board to search for a solution
+	 * @param 	depth   the current depth of the board being searched
+	 * @return	true,	if a solution has been found
 	 */
 	public boolean btDFS(nQueenBoard board, int depth) {
 
+		//Increment number of nodes expanded
 		numNodes++;
 
+		//Check if the board is a solution
+		//If so, print the solution.
 		if(board.getCollisions() == 0) {
 			System.out.println("Backtracking DFS found solution.");
 			System.out.println(board.toString());
 			return true;
 		}
 
-
+		//If board is not a solution, but at the bottom of the
+		//tree, it is not a solution.
 		if (depth == board.getSize()) {
 			return false;
 		}
 		else {
+			//Loop through all positions of a queen at a given depth
 			for (int i = 0; i < board.getSize(); i++) {
+				//Create a new board modified at one position
 				nQueenBoard b = new nQueenBoard(board, depth, i);
 
+				//This outer-if is the backtracking
+				//Only expand the node if the choice currently satisfies constraints
 				if (!b.hasCollisionsUpToDepth(depth + 1)) {
+					//Expand the node with a recursive call
 					if (btDFS(b, depth+1)) {
 						return true;
 					}
@@ -87,11 +114,18 @@ public class DFSAlgorithms {
 		}
 	}
 
+
 	/**
+	 * Recursive method for DFS with forward checking. 
+	 * This implementation tracks the domain values of 
+	 * each row in the board, pruning the domains of queens
+	 * at greater depths, based on the queen just placed.
+	 * If a domain of a queen's row becomes empty, that board's
+	 * node is not expanded.
 	 * 
-	 * @param board
-	 * @param depth
-	 * @return
+	 * @param 	board	the initial board to search for a solution
+	 * @param 	depth   the current depth of the board being searched
+	 * @return	true,	if a solution has been found
 	 */
 	public boolean fcDFS(nQueenBoard board, int depth) {
 
