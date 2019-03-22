14b2e74bd3d6dbd5692ccb9c4fe18564b26af08b
GCC\TANMR1 <tanmr1@gcc.edu>
2019-03-22 06:58:10 -0400

    hopefully finished LSRR

--- src/localRandRestartSearch.java
+++ src/localRandRestartSearch.java
@@ -6,31 +6,34 @@
  */
 public class localRandRestartSearch {
     public static int numRestarts = 0;
+    public static int numNodes = 0;
     public static double totalTime = 0.0;
     public static int minCollisions = 0;
-    public static int betterBoard = new nQueenBoard;
-    public static int betterLocation = -1;
+    public static int betterBoard = null;
+    public static boolean betterBoardExists = true;
 
-    private nQueenBoard board;   		
+    private nQueenBoard board;
 
     /** constructor: takes board
      *
      * @param board
      */
-    public nQueenBoard(nQueenBoard startBoard) {
+    public localRandRestartSearch(nQueenBoard startBoard) {
         this.board = startBoard;
+        this.search();
     }
 
     /**
      * @return found solution
      */
-    public static boolean localRandRestartSearch() {
+    public static boolean search() {
 
-        minCollisions = board.getCollsions();
+        minCollisions = board.getCollisions();
 
         // while there are still better options keep optimizing
-        while (better) {
-
+        while (betterBoardExists) {
+            betterBoardExists = betterNeighbor();
+            this.board = betterBoard;
         }
         // there are no better options for the board
         // if there are no collisions then the solution has been found
@@ -45,25 +48,35 @@ public class localRandRestartSearch {
         // otherwise rand restart because we're in a local min/max
         else {
             numRestarts++;
+            numNodes++;
+            int s = this.board.size;
+            this.board = new nQueenBoard(s);
         }
 
 
     }
 
     // generate all neighbors and find minimum collisions board
-    public static boolean betterNeighbor(nQueenBoard board) {
+    public static boolean betterNeighbor() {
+
+        boolean foundBetter = false;
 
         for (int q = 0; q < board.size; q++) {
 
             for (int p = 0; p < board.size; p++) {
+                numNodes++;
 
-                nQueenBoard temp = new nQueenBoard(board, q, p);
+                nQueenBoard temp = new nQueenBoard(this.board, q, p);
                 // if this has fewer collisions than the
                 if(temp.getCollisions() < minCollisions) {
 
+                    betterBoard = temp;
+                    minCollisions = temp.getCollisions();
+                    foundBetter = true;
                 }
             }
         }
+        return foundBetter;
     }
 
 }
